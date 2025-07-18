abae95e88e2632faa68d0222da3cc3bc0428b207
Thgzin <thgziin@outlook.com>
2025-07-18 20:08:49 -0300

    javascriptExercises.2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+/*const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
 const whisper = message.toLowerCase().trim();
 console.log(whisper);
+*/
+
+const word = "skateboard"; //Don't change this line!
+
+const changeWord = word.slice(5);
+const facialHair = changeWord.replace("o", "e");
+
+console.log(facialHair);
