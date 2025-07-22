506abdb3d5495a95e974de572dc6ca18539507eb
Thgzin <thgziin@outlook.com>
2025-07-22 18:33:10 -0300

    javascriptExercises.6

--- js/index.js
+++ js/index.js
@@ -39,6 +39,7 @@ function isOdd(num) {
 isOdd(3);
 */
 
+/*
 function getColor(phrase) {
   if (phrase === "stop") {
     console.log("red");
@@ -52,3 +53,20 @@ function getColor(phrase) {
 }
 
 getColor("stop");
+*/
+
+// Change the value of num, so that "YOU GOT ME!" prints out
+const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :)
+
+// DO NOT TOUCH ANYTHING BELOW (please)
+if (num <= 100) {
+  if (num >= 50) {
+    console.log("HEY!");
+  }
+} else {
+  if (num < 103) {
+    if (num % 2 === 0) {
+      console.log("YOU GOT ME!");
+    }
+  }
+}
