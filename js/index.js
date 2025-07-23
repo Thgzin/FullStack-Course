83038fde6a1af2aff19634c410495ef87380d0e2
Thgzin <thgziin@outlook.com>
2025-07-23 20:30:25 -0300

    javascriptExercises.13

--- js/index.js
+++ js/index.js
@@ -134,7 +134,18 @@ const restaurant = {
 const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
 console.log(fullAddress);
 */
-
+/*
 for (let i = 1; i <= 6; i++) {
   console.log("Da ba dee da ba daa");
+}
+
+/*
+for (let i = 25; i >= 0; i-=5) {
+  console.log(i);
+}
+*/
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].toUpperCase());
 }
