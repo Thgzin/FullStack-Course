ccfb5fa3b21386ee799d52d53531bc7894a94fe6
Thgzin <thgziin@outlook.com>
2025-07-22 21:47:02 -0300

    javascriptExercises.10

--- js/index.js
+++ js/index.js
@@ -92,7 +92,7 @@ const leaderboard = ["Harry", "Lua", "Hermione", "Bellatrix"]; //DON'T TOUCH THI
 leaderboard[1] = "Luna";
 leaderboard[3] = "Draco";
 */
-
+/*
 const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"]; //DO NOT TOUCH THIS LINE!
 
 // YOUR CODE GOES BELOW THIS LINE:
@@ -100,3 +100,24 @@ planets.shift();
 planets.push("Saturn");
 planets.unshift("Mercury");
 console.log(planets);
+*/
+
+/*
+const airplaneSeats = [
+  ["Ruth", "Anthony", "Stevie"],
+  ["Amelia", "Pedro", "Maya"],
+  ["Xavier", "Ananya", "Luis"],
+  ["Luke", null, "Deniz"],
+  ["Rin", "Sakura", "Francisco"],
+];
+airplaneSeats[3][1] = "Hugo";
+console.log(airplaneSeats);
+*/
+
+  const product = {
+    name: "Gummy Bears",
+    inStock: true,
+    price: 1.99,
+    flavors: ["grape", "apple", "cherry"],
+  };
+  console.log(product);
