59aaf65f949412bba8bd42e3ac015ba69d1f9057
Thgzin <thgziin@outlook.com>
2025-07-23 23:58:59 -0300

    guessingGame.build-2

--- js/guessingGame.js
+++ js/guessingGame.js
@@ -1,4 +1,17 @@
-let maximum = parseInt(prompt("enter the maximum number"));
+let maximum = parseInt(prompt("enter the maximum number!"));
+while (!maximum || maximum <= 0) {
+  maximum = parseInt(prompt("enter a valid number!"));
+}
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+while (guess !== targetNum) {
+  if (guess > targetNum) {
+    guess = parseInt(prompt("Too high! Enter a new guess"));
+  } else {
+    guess = parseInt(prompt("Too low! Enter a new guess"));
+  }
+}
+console.log("YOU GOT IT");
