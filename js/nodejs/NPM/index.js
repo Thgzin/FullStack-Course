677d70ab10eccbec470c11bd84a0b186d24e8da3
Thgzin <thgziin@outlook.com>
2025-08-11 22:53:41 -0300

    installing packages w/node.2

--- js/nodejs/NPM/index.js
+++ js/nodejs/NPM/index.js
@@ -1,6 +1,7 @@
 var giveMeAJoke = require("give-me-a-joke");
+const colors = require("colors");
 
 // To get a random dad joke
 giveMeAJoke.getRandomDadJoke(function (joke) {
-  console.log(joke);
+  console.log(joke.rainbow);
 });
