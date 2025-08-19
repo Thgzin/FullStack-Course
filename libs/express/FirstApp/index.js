9a77661e2603297752c53c7c68115d950ab0e1fe
Thgzin <thgziin@outlook.com>
2025-08-18 23:44:28 -0300

    working w/nodemon

--- libs/express/FirstApp/index.js
+++ libs/express/FirstApp/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 //});
 
 app.get("/", (req, res) => {
-  res.send("<h1>This is the home page!</h1>");
+  res.send("<h1>This is the home page!!!</h1>");
 });
 
 app.get("/cats", (req, res) => {
